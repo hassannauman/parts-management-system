54c17749b7d2ce094d6a20acb9f3c6e1ca955187
Hassan Nauman <73989560+hassannauman@users.noreply.github.com>
2026-04-13 12:56:33 +0500

    Working backend service with GET request for parts

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/parts', async (req, res) => {
+  const result = await pool.query('SELECT * FROM parts');
+  res.json(result.rows);
+});
+
+app.listen(3000, () => console.log('Server running'));
